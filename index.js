d56894d3fe3b78e374afe597661ea007ba4c7e15
milk9111 <connorlndbrg349@gmail.com>
2020-12-20 15:29:56 -0500

    Start of game
    Able to load a start topic, search for topics, and render wikipedia HTML on game board

---
+++ index.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+const mediaTypes = {
+    zip: 'application/zip',
+    jpg: 'image/jpeg',
+    html: 'text/html',
+    css: 'text/css',
+    js: 'text/javascript'
+  }
+
+http.createServer(function (req, res) {
+    console.log(req.method + ' ' + req.url)
+
+    let url = req.url;
+    if (url === "" || url === "/" || url === "/?" || url === undefined || url === null) {
+        url = "/index.html";
+    }
+
+    req.url = url;
+
+    const filepath = path.join('public', url)
+
+    fs.readFile(filepath, function(err, data) {
+        if (err) {
+            res.statusCode = 404
+            return res.end('File not found or you made an invalid request.');
+        }
+    
+        let mediaType = 'text/html'
+        const ext = path.extname(filepath)
+        if (ext.length > 0 && mediaTypes.hasOwnProperty(ext.slice(1))) {
+            mediaType = mediaTypes[ext.slice(1)]
+        }
+
+        res.writeHead(200, {'Content-Type': mediaType, 'Access-Control-Allow-Origin': '*'});
+        res.write(data);
+
+        return res.end();
+  });
+}).listen(8080); 
+
+console.log("Started server");
---
+++ public/js/wikipedia-game.js
@@ -0,0 +1,96 @@
+
+$('.toast').toast({
+    'autohide': false
+});
+
+jQuery(function() {
+    $("a").on("click", function(event) {
+        var href = $(this).attr("href");
+        if (href.substring(0, 6) === "/wiki/") {
+            event.preventDefault();
+            setGameBoard(href.substring(6));
+        }
+    });
+});
+
+
+function searchArticle(search) {
+    if (search === "" || search === null || search === undefined) {
+        return;
+    }
+
+    let topic = encodeURIComponent(search);
+
+    $("#loadingSpinnerSearch").removeClass("d-none");
+
+    $.ajax(
+        {
+            url: "https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=prefixsearch&pssearch=" + topic, 
+            success: function(result, status, xhr){
+                if (xhr.status !== 200 || result === null || result.query === null || result.query.prefixsearch === null || result.query.prefixsearch.length === 0 || !topicExists(search, result.query.prefixsearch)) {
+                    $("#searchToast").css("background-color", "red");
+                    $("#searchToastHeader").text("Failure");
+                    $("#searchToastBody").text("Could not find an article for the given topic.");
+                } else {
+                    $("#searchToast").css("background-color", "green");
+                    $("#searchToastHeader").text("Success");
+                    $("#searchToastBody").text("That topic exists!");
+                }
+
+                $('#searchToast').toast('show');
+            }
+        }
+    ).done(function(msg) {
+        $("#loadingSpinnerSearch").addClass("d-none");
+    });
+}
+
+function startGame(start, target) {
+    // TODO - Validate that start topic exists and show error if not
+
+    // TODO - Validate that target topic exists and show error if not
+
+    setGameBoard(start);
+}
+
+function setGameBoard(topic) {
+    $("#gameContent").addClass("d-none");
+    $("#gameBounds").removeClass("d-none");
+
+    $("#loadingSpinner").removeClass("d-none");
+
+    $.ajax({
+        method: "GET",
+        url: "https://en.wikipedia.org/w/api.php?origin=*&action=parse&format=json&prop=text&formatversion=2&page=" + encodeTopic(topic),
+        success: function(result, status, xhr) {
+            if (xhr.status !== 200 || result === null || result.parse === null || result.parse.text === null || result.parse.text === "") {
+                alert("Invalid start topic");
+                return;
+            }
+
+            $("#gameContent").removeClass("d-none");
+            $("#gameContent").empty();
+            $("#gameContent").append(result.parse.text);
+        }
+    }).done(function(msg){
+        $("#loadingSpinner").addClass("d-none");
+    });
+}
+
+function encodeTopic(topic) {
+    return topic.replace(" ", "_");
+}
+
+function topicExists(topic, searchResults) {
+    let found = false;
+
+    searchResults.forEach(function(item, index) {
+        if (found) {
+            return;
+        }
+
+        found = item.title.toLowerCase() === topic.toLowerCase();
+    });
+
+    return found;
+}
